71c00b3f9bd96d87634309ceb45876a0ee7b8a09
FCH1984 <103128081+FCH1984@users.noreply.github.com>
2023-11-04 14:12:10 -0700

    Add files via upload

---
+++ TPJS/test.js
@@ -0,0 +1,12 @@
+
+var texto1 = "Conf Bs As" ;
+var texto2 = "La conferencia";
+var texto3 = "Las Oradores";
+var texto4 = "El lugar y las Fechas";
+var texto5 = "Conviertete en orador";
+var texto6 = "Comprar tickets";
+
+var concatenacion = texto1 + " " + texto2 + " " + texto3 + " "+ texto4  + " " + texto5 + " "+ texto6;
+
+document.write (concatenacion)
+
